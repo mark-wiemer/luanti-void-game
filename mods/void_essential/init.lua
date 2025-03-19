a076d1d6aa9e72ef6a97734c5d839ee7517b6738
Mark Wiemer <markwiemer@outlook.com>
2025-03-18 22:23:33 -0700

    Simplify code

--- mods/void_essential/init.lua
+++ mods/void_essential/init.lua
@@ -1,29 +1,14 @@
 local register_node = core.register_node
 local register_alias = core.register_alias
 
+local node_types = { 'stone', 'water_source', 'river_water_source' }
 
-register_node('void_essential:stone', {
-    description = 'Essential node for mapgen alias “mapgen_stone”',
-    tiles = { 'void_essential_stone.png' },
-    groups = { oddly_breakable_by_hand = 3 },
-    is_ground_content = true
-})
-
-register_node('void_essential:water_source', {
-    description = 'Essential node for mapgen alias “mapgen_water_source”',
-    tiles = { 'void_essential_water_source.png' },
-    groups = { oddly_breakable_by_hand = 3 },
-    is_ground_content = true
-})
-
-register_node('void_essential:river_water_source', {
-    description = 'Essential node for mapgen alias “mapgen_river_water_source”',
-    tiles = { 'void_essential_river_water_source.png' },
-    groups = { oddly_breakable_by_hand = 3 },
-    is_ground_content = true
-})
-
-
-register_alias('mapgen_stone', 'void_essential:stone')
-register_alias('mapgen_water_source', 'void_essential:water_source')
-register_alias('mapgen_river_water_source', 'void_essential:river_water_source')
+-- register each node type in a loop
+for _, node_type in ipairs(node_types) do
+    register_node('void_essential:' .. node_type, {
+        description = 'Essential node for mapgen alias “mapgen_' .. node_type .. '”',
+        tiles = { 'void_essential_' .. node_type .. '.png' },
+        groups = { default = 1 },
+    })
+    register_alias('mapgen_' .. node_type, 'void_essential:' .. node_type)
+end
