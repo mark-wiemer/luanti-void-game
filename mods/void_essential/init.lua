758d80ac2b0629ba528a2852d04ee97613a99189
Mark Wiemer <markwiemer@outlook.com>
2025-03-19 22:26:02 -0700

    Remove fancy quotes, remove comment

--- mods/void_essential/init.lua
+++ mods/void_essential/init.lua
@@ -3,10 +3,9 @@ local register_alias = core.register_alias
 
 local node_types = { 'stone', 'water_source', 'river_water_source' }
 
--- register each node type in a loop
 for _, node_type in ipairs(node_types) do
     register_node('void_essential:' .. node_type, {
-        description = 'Essential node for mapgen alias “mapgen_' .. node_type .. '”',
+        description = 'Essential node for mapgen alias "mapgen_' .. node_type .. '"',
         tiles = { 'void_essential_' .. node_type .. '.png' },
         groups = { default = 1 },
     })
