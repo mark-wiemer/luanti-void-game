14dff583e650d83d9b6e2bceda460f73baa4f974
Mark Wiemer <markwiemer@outlook.com>
2025-03-19 22:56:23 -0700

    Format init.lua

--- mods/void_hand/init.lua
+++ mods/void_hand/init.lua
@@ -1,14 +1,13 @@
 local register_item = core.register_item
 
-
 register_item(':', {
     type = 'none',
     wield_image = 'void_hand_hand.png',
-    wield_scale = {x = 0.5, y = 1, z = 4},
+    wield_scale = { x = 0.5, y = 1, z = 4 },
     tool_capabilities = {
         groupcaps = {
             default = {
-                times = {[1] = 0},
+                times = { [1] = 0 },
                 uses = 0,
             },
         },
