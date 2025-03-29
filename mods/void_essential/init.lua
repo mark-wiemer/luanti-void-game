f00a8ff1afee2e418f2f44e3a037263de359a7f8
Mark Wiemer <markwiemer@outlook.com>
2025-03-29 12:18:38 -0700

    Stop tracking auto-generated Lua files

--- mods/void_essential/init.lua
+++
@@ -1,9 +0,0 @@
---[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
-local ____exports = {}
-local node_types = {"stone", "water_source", "river_water_source"}
-for ____, node_type in ipairs(node_types) do
-    local node_name = "void_essential:" .. node_type
-    core.register_node(node_name, {description = ("Essential node for mapgen alias \"mapgen_" .. node_type) .. "\"", tiles = {("void_essential_" .. node_type) .. ".png"}, groups = {default = 1}})
-    core.register_alias("mapgen_" .. node_type, node_name)
-end
-return ____exports
--- mods/void_hand/init.lua
+++
@@ -1,4 +0,0 @@
---[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
-local ____exports = {}
-core.register_item(":", {type = "none", wield_image = "void_hand_hand.png", wield_scale = {x = 0.5, y = 1, z = 4}, tool_capabilities = {groupcaps = {default = {times = {0}}}}})
-return ____exports
